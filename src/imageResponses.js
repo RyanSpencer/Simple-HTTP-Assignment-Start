a147367f14efb5d0b68f31d745f93096465a953a
Ryan Spencer <rxs2833@g.rit.edu>
2017-09-06 20:57:44 -0400

    Typo in imageResponses

--- src/imageResponses.js
+++ src/imageResponses.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const pic = fs.readFileSync('{__dirname}/../client/spongegar.png');
+const pic = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
 
 const getPic = (request, response) => {
   response.writeHead(200, { 'Content-Type': 'image/png' });
